b5d0c9d915b4419153a553d600bdda2d3f0939cf
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:20:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,11 +5,15 @@ def second_challenge
       matriarch: {}
       hero: {}
       hero_friends: []
-      #your key/value pairs here!
+      
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      patriarch: {}
+      matriarch: {}
+      heroine: {}
+      heroine_friends: []
+      }
+  }
 
   
 end
