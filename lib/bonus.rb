b0df1afd6cfb7948d4932ab74cf672038b426774
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:29:03 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  epic_tragedy[:montague][:]
+  epic_tragedy[:montague][:hero][:status] = ''
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
