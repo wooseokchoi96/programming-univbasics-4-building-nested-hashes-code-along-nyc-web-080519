d3a98b435a7f8537e4cb3e2ddb1ca3c78001c6e5
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:19:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      patriarch: {}
+      matriarch: {}
+      hero: {}
+      hero_friends: []
       #your key/value pairs here!
       }, 
     :capulet => {
